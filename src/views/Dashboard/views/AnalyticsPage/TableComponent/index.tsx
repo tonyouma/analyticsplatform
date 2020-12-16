28391c2e01bb915e70f4e5a95d7fd289e4c8fb5a
TonyOuma <kyrietony@gmail.com>
2020-12-16 23:49:59 +0300

    feat: add table component

---
+++ src/views/Dashboard/views/AnalyticsPage/TableComponent/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+
+import TableView from './TableView';
+
+const StaffListView = () => {
+  return (
+    <>
+      <Box mt={3}>
+        <TableView />
+      </Box>
+    </>
+  );
+};
+
+export default StaffListView;
